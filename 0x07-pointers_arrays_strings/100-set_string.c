6238c64cc86bf12eadb09857b5c004a664f762d1
Strin6s <esebaby50@gmail.com>
2023-01-03 04:31:01 -0800

    correct

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -6,7 +6,7 @@ include "main.h"
  *
  * Return: Always 0.
  */
-void set_string(char **s, char *to)
+void set_string(char **s, char*to)
 {
 	*s = to;
 }
